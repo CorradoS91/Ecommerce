0c5216cf67e9a0d29d2d748509705dbe0a799905
corra <corradostella@yahoo.it>
2025-01-28 13:16:10 +0100

    Added Repositories

---
+++ src/main/java/it/corrado/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package it.corrado.repository;
+
+import it.corrado.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> {
+}
